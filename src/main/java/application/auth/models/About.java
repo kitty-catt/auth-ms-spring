433f43536f2e64a49098b9987cd6206b9b0f2719
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-07-17 11:52:52 -0500

    reformatted code, moved classes to their appropriate packages

--- src/main/java/application/auth/models/About.java
+++ src/main/java/application/auth/models/About.java
@@ -1,51 +1,49 @@
 package application.auth.models;
 
 public class About {
-	
-	private String name;
 
-	private String parentRepo;
+    private String name;
 
-	private String description;
-	
-	public About() {
-		
-	}
-	
-	public About(String name, String parentRepo, String description) {
-		this.setName(name);
-		this.setParentRepo(parentRepo);
-		this.setDescription(description);
-	}
+    private String parentRepo;
 
-	public String getName() {
-		return name;
-	}
+    private String description;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public About() {
 
-	public String getParentRepo() {
-		return parentRepo;
-	}
+    }
 
-	public void setParentRepo(String parentRepo) {
-		this.parentRepo = parentRepo;
-	}
+    public About(String name, String parentRepo, String description) {
+        this.setName(name);
+        this.setParentRepo(parentRepo);
+        this.setDescription(description);
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String toString() {
-		return "About [name=" + name + ", parentRepo=" + parentRepo + ", description=" + description + "]";
-	}
-	
+    public String getParentRepo() {
+        return parentRepo;
+    }
 
+    public void setParentRepo(String parentRepo) {
+        this.parentRepo = parentRepo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "About [name=" + name + ", parentRepo=" + parentRepo + ", description=" + description + "]";
+    }
 }
