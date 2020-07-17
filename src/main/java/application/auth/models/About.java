778c7f31927126171701e771904df664cfb69244
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-07-17 12:03:40 -0500

    updated models to use lombok

--- src/main/java/application/auth/models/About.java
+++ src/main/java/application/auth/models/About.java
@@ -1,45 +1,20 @@
 package application.auth.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class About {
 
-    private String name;
+    @Getter @Setter private String name;
 
-    private String parentRepo;
+    @Getter @Setter private String parentRepo;
 
-    private String description;
-
-    public About() {
-
-    }
+    @Getter @Setter private String description;
 
     public About(String name, String parentRepo, String description) {
         this.setName(name);
         this.setParentRepo(parentRepo);
         this.setDescription(description);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getParentRepo() {
-        return parentRepo;
-    }
-
-    public void setParentRepo(String parentRepo) {
-        this.parentRepo = parentRepo;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     @Override
--- src/main/java/application/auth/models/Customer.java
+++ src/main/java/application/auth/models/Customer.java
@@ -1,36 +1,13 @@
 package application.auth.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import lombok.Getter;
+import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Customer {
 
-    private String customerId;
-    private String username;
-    private String password;
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	@Getter @Setter private String customerId;
+	@Getter @Setter private String username;
+	@Getter @Setter private String password;
 
 }
